9a65c3a620e36c6e36fefba5c50ed7af8b58e305
Martina Capone <mcapone@cybsec.it>
2025-07-02 21:42:35 +0000

    Resolve bug affecting model execution during inference

--- src/core/model_anita.py
+++ src/core/model_anita.py
@@ -3,7 +3,7 @@ from src.utils.config import Config
 
 def load_llm_anita():
     return Llama(
-        model_path=Config.ANITA_PATH,
+        model_path=str(Config.ANITA_PATH),
         n_ctx=Config.N_CTX,
         n_threads=Config.N_THREADS,
         n_gpu_layers=Config.N_GPU_LAYERS,
